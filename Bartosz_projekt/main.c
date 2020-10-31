6d7d26955c32b5a0f8c79d3b7f707908497b9e25
batotnv <bartosznguyenvan@gmail.com>
2020-10-31 23:22:40 +0100

    lekki progres v2

--- Bartosz_projekt/main.c
+++ Bartosz_projekt/main.c
@@ -10,25 +10,29 @@
 
 //kmeans parameters
 
-#define number_clusters 2
+#define number_clusters 3
 #define MAX_ITERATIONS 100
 
 
-typedef struct
-{
-  double *values;
-  int capacity;
-} myArray;
+struct myArray{
+  char observation_name[50];//nazwa obserwacji
+  double values[50];//miejsce na dane
+  int cluster; //przyporzadkowany klaster
+};
+
+struct vector{
+    double values[50];
+};
 
 
 //function that computes euclidean distance
-double euclidean_distance(double *p, double *q, int n){
+double euclidean_distance(struct myArray p, struct myArray q, int n){
 
     double dist;
     int i;
 
     for(i = 0; i < n; i++){
-        dist += (p[i]-q[i])*(p[i]-q[i]);
+        dist += (p.values[i]-q.values[i])*(p.values[i]-q.values[i]);
         printf("%lf \n", dist);
     }
     dist = sqrt(dist);
@@ -73,18 +77,46 @@ void print_array(int row, int col, double matrix[row][col]){
     }
 }
 
-//double calculate_centroid()
+//funkcja liczaca centroidy dla poszczegolnych klastrow
+
+struct vector calculate_centroid(int length, int column,struct myArray *array){
+
+    struct vector centroid;
+    double mean;
+    int i,j;
+
+//length = 3
+//column = 5
+
+    for (i = 0; i < column; i++){
+        for(j = 0; j < length; j++){
+            mean += array[i].values[j];
+            printf("Suma: %f \n", mean);
+            }
+        mean = mean/length;
+        centroid.values[i] = mean;
+    }
+    return centroid;
+}
+
+
+//int ile to jest maksymalna ilosc danych
+
+
 
 
 int main()
 {
     srand(time(NULL));
-    int lineCount = 0;
+    int lineCount = 1;
     double testArray[3][5];
+    struct myArray newArray[lineCount];
   //  char ch_arr[3][20];
     int i, j, k;
-
+    double value = 0;
     FILE *fpointer = fopen("data.txt", "r");
+
+
 
     //returning error when program cannot open a file
     if (!fpointer){
@@ -92,21 +124,22 @@ int main()
         return 1;
     }
 
-    while( !feof(fpointer))
-    {
-        for( i = 0; i < 3; i++){
-    //        fscanf(fpointer,"%ls,", &ch_arr[i]);
-            for(j = 0; j < 5; j++){
-                    fscanf(fpointer,"%lf,", &testArray[i][j]);
-            }
-        }
-     }
+      for( i = 0; i < 3; i++){
 
+            for(j = 0; j < 5; j++){
+                    //fscanf(fpointer,"%lf", &testArray[i][j]);
+                    fscanf(fpointer,"%lf", &newArray[i].values[j]);
+            }
+            lineCount++;
+        }
     fclose(fpointer);
+
 
     for(k = 0; k < 3; k++){
         for(j = 0; j < 5; j++){
-            printf("%f ", testArray[k][j]);
+         //   printf("%f ", testArray[k][j]);
+
+            printf("%lf\n", newArray[k].values[j]);
         }
         printf("\n");
     }
@@ -125,9 +158,9 @@ int main()
     //double test2[] = {1, 3, 5, 6, 9};
 
     //przekazywanie poszczegolnych wierszy dziala dobrze
-    printf("Euclidean dist: %lf", euclidean_distance(testArray[0],testArray[1],column));
+    //printf("Euclidean dist: %lf", euclidean_distance(testArray[0],testArray[1],column));
 
-
+    printf("Euclidean dist: %lf", euclidean_distance(newArray[0],newArray[1],column));
 
 //    double przydzielone[][] = assign_to_clusters(length, column, testArray)
 
@@ -136,6 +169,33 @@ int main()
 
 
     //printf("asdad: %f", elo[0]);
+
+    //losowe przydzielanie klastrow
+
+    printf("\n\n");
+  //  printf("liczba wierszy: %d", lineCount-1);
+
+    for(i = 0; i < length; i++){
+
+        newArray[i].cluster = losowa(1,number_clusters);
+        printf("Obserwacja nr %d nalezy do klastra %d \n", i, newArray[i].cluster);
+    }
+
+
+    //centroida
+    //double centroid[length] = calculate_centroid(newArray, length, column);
+
+
+//    double centroid[length] = calculate_centroid(newArray, length, column);
+//
+
+
+    struct vector centroid = calculate_centroid(length, column, newArray);
+
+    for (i = 0; i < column; i++){
+        printf("%f \n", centroid.values[i]);
+    }
+
 
     return 0;
 }
